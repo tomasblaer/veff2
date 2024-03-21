c1668ea7e62f6ef04ca9f190ff128226362fce38
tomasblaer <tomasblaer@gmail.com>
2024-03-21 23:35:39 +0000

    readme og index

--- verk4/README.md
+++ verk4/README.md
@@ -1,3 +1,5 @@
 # Vefforritun 2 2024, verkefni 4
 
 ### Verkefnið er hýst á vercel [hér](https://veff2.vercel.app/)
+
+Tómas Blær Guðmundsson - tbg16@hi.is
--- verk4/src/app/page.tsx
+++ verk4/src/app/page.tsx
@@ -3,7 +3,11 @@ import { Metadata } from "next";
 export default async function Home() {
   return (
     <main className="flex justify-center">
-      <h1>Leikirnir</h1>
+      <h1 className="text-xl pt-10">
+        <b>Leikjavefurinn</b>, vefur fyrir yfirferð og breytingu
+        á leikjagögnum, smellið á Leikir uppi í hægra horni
+        til þess að skoða og fara yfir leiki.
+      </h1>
     </main>
   );
 }
